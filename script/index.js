7425ce86677380a6780a0770d209b432bce031e7
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2023-06-26 15:28:49 +0200

    Responsive :+1:

--- script/index.js
+++ script/index.js
@@ -1,12 +1,19 @@
-// Supprime le marginTop de la navbar lors du scroll sur la page
 const navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', function() {
    if (window.scrollY > 5) {
      navbar.classList.add('navbar-transition');
-     navbar.style.marginTop = '0';
+     if (window.innerWidth < 980) {
+       navbar.style.marginTop = '20px';
+     } else {
+       navbar.style.marginTop = '0';
+     }
    } else {
      navbar.classList.add('navbar-transition');
-     navbar.style.marginTop = '';
+     if (window.innerWidth < 980) {
+       navbar.style.marginTop = '20px';
+     } else {
+       navbar.style.marginTop = '38px';
+     }
  
      setTimeout(function() {
        navbar.classList.remove('navbar-transition');
