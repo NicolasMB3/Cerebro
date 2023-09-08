12d9139efca851497c41b85f4addc6043e362e96
LucasMadrangesIntuition <lucas@agence-intuition.fr>
2023-09-08 12:02:23 +0200

    Page de connexion
    Ajout du script pour le bouton du mot de passe.
    Modification du mode focus pour les input du formulaire et du footer.
    Ajout d'une image svg pour le fond de la page de connexion.
    Ajout de la classe cbro-badge-pink.
    Ajout de la classe btn_dark_green.

---
+++ script/mdp_btn.js
@@ -0,0 +1,11 @@
+function eye_on() {
+  document.getElementById("eye_on").style.display = "none";
+  document.getElementById("eye_off").style.display = "inline-block";
+  document.getElementById("password").type = "password";
+}
+
+function eye_off() {
+  document.getElementById("eye_off").style.display = "none";
+  document.getElementById("eye_on").style.display = "inline-block";
+  document.getElementById("password").type = "text";
+}
