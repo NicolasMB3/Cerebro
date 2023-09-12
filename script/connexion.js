a38979f50e954d03bd672db8f8188311ace6b037
LucasMadrangesIntuition <lucas@agence-intuition.fr>
2023-09-12 17:15:11 +0200

    Page de connexion
    Responsive pour le formulaire de connexion.
    Ajout du script pour les input de password de la page de changement de mot de passe.

--- script/connexion.js
+++ script/connexion.js
@@ -1,7 +1,12 @@
-// Bouton Oeil ouvert / fermé du mot de passe
-const eyeOn = document.getElementById("eye_on");
-const eyeOff = document.getElementById("eye_off");
-const password = document.getElementById("password");
+// Bouton Oeil ouvert / fermé du mot de passe de connexion
+const eyeOn1 = document.getElementById("eye_on1");
+const eyeOff1 = document.getElementById("eye_off1");
+
+// Bouton Oeil ouvert / fermé du changement de mot de passe
+const eyeOn2 = document.getElementById("eye_on2");
+const eyeOff2 = document.getElementById("eye_off2");
+const eyeOn3 = document.getElementById("eye_on3");
+const eyeOff3 = document.getElementById("eye_off3");
 
 // Section
 const connexionHero = document.getElementById("connexion_hero");
@@ -12,24 +17,76 @@ const resetMdp = document.getElementById("reset_mdp");
 // Bouton pour changer de section
 const BtnForgetMdp = document.getElementById("btn_forget_mdp");
 const BtnEmail = document.getElementById("btn_email");
-const BtnConnexion = document.getElementById("btn_connexion");
+const BtnReturnConnexion = document.getElementById("btn_return_connexion");
 const BtnResetMdp = document.getElementById("btn_reset_mdp");
+const BtnResendEmail = document.getElementById("btn_resend_email");
+const BtnConnexion = document.getElementById("btn_connexion");
+
+// Input
+const InputEmail = document.getElementById("email");
+const InputPassword1 = document.getElementById("password1");
+const InputPassword2 = document.getElementById("password2");
+const InputPassword3 = document.getElementById("password3");
+
+// Regex
+const RegexEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
 
 // Dsiplay none / inline-block et type password / text
-function HideOff() {
-  eyeOff.classList.add("d-none");
-  eyeOn.classList.remove("d-none");
-  password.type = "text";
+function HideOff1() {
+  eyeOff1.classList.add("d-none");
+  eyeOn1.classList.remove("d-none");
+  InputPassword1.type = "text";
 }
 
-function HideOn() {
-  eyeOff.classList.remove("d-none");
-  eyeOn.classList.add("d-none");
-  password.type = "password";
+function HideOn1() {
+  eyeOff1.classList.remove("d-none");
+  eyeOn1.classList.add("d-none");
+  InputPassword1.type = "password";
 }
 
-eyeOff.addEventListener("click", HideOff);
-eyeOn.addEventListener("click", HideOn);
+eyeOff1.addEventListener("click", HideOff1);
+eyeOn1.addEventListener("click", HideOn1);
+
+function HideOff2() {
+  eyeOff2.classList.add("d-none");
+  eyeOn2.classList.remove("d-none");
+  InputPassword2.type = "text";
+}
+
+function HideOn2() {
+  eyeOff2.classList.remove("d-none");
+  eyeOn2.classList.add("d-none");
+  InputPassword2.type = "password";
+}
+
+eyeOff2.addEventListener("click", HideOff2);
+eyeOn2.addEventListener("click", HideOn2);
+
+function HideOff3() {
+  eyeOff3.classList.add("d-none");
+  eyeOn3.classList.remove("d-none");
+  InputPassword3.type = "text";
+}
+
+function HideOn3() {
+  eyeOff3.classList.remove("d-none");
+  eyeOn3.classList.add("d-none");
+  InputPassword3.type = "password";
+}
+
+eyeOff3.addEventListener("click", HideOff3);
+eyeOn3.addEventListener("click", HideOn3);
+
+// Fonction pour vérifier les input avant la connexion
+function TestRegex() {
+  const Email = InputEmail.value;
+  const found = Email.match(RegexEmail);
+  console.log(found);
+  //null = false
+  //non null = true
+}
+
+BtnConnexion.addEventListener("click", TestRegex);
 
 // Fonction pour chacher la section Connexion et affiché la section Mot de passe oublié
 function HideSectionConnexion() {
@@ -45,8 +102,8 @@ function HideSectionMdp1() {
   sendMail.classList.remove("d-none");
 
   // Fonction Timer pour la page d'email envoyé
-  const departHeures = 60;
-  let temps = departHeures * 60;
+  const departMinutes = 60;
+  let temps = departMinutes * 60;
   const timerElement = document.getElementById("timer");
   setInterval(() => {
     let heures = parseInt(temps / 3600, 10);
@@ -57,6 +114,14 @@ function HideSectionMdp1() {
     secondes = secondes < 10 ? "0" + secondes : secondes;
     timerElement.innerText = `${heures}:${minutes}:${secondes}`;
     temps = temps <= 0 ? 0 : temps - 1;
+
+    if (temps !== 0) {
+      BtnResendEmail.disabled = true;
+      BtnResendEmail.style.opacity = "0.5";
+    } else {
+      BtnResendEmail.disabled = false;
+      BtnResendEmail.style.opacity = "1";
+    }
   }, 1000);
 }
 
@@ -68,7 +133,7 @@ function HideSectionMdp2() {
   connexionHero.classList.remove("d-none");
 }
 
-BtnConnexion.addEventListener("click", HideSectionMdp2);
+BtnReturnConnexion.addEventListener("click", HideSectionMdp2);
 
 // Fonction pour cacher la seciton changement de mot passe et afficher la section Connexion
 function HideSectionResetMdp() {
