558e6a308a4c2e332aa300907402da6f4936ecd8
LucasMadrangesIntuition <lucas@agence-intuition.fr>
2023-09-26 16:43:29 +0200

    Ajout de la page de contact, des animations et du style (desktop + responsive).

---
+++ script/contact.js
@@ -0,0 +1,76 @@
+const btnContact = document.getElementById("btn_contact");
+const toast = document.getElementById("liveToast");
+const btnCloseToast = document.getElementById("btn-close");
+
+const copyClipBoard = document.getElementById("copy_clipboard");
+const copyIcon = document.getElementById("icon_copy");
+const checkIcon = document.getElementById("icon_check");
+
+btnContact.addEventListener("click", HideOffToast);
+btnCloseToast.addEventListener("click", HideOnToast);
+
+function HideOffToast() {
+  toast.style.display = "block";
+
+  anime({
+    targets: toast,
+    translateY: "-250px",
+    duration: 500,
+    easing: "easeInOutExpo",
+  });
+}
+
+function HideOnToast() {
+  anime({
+    targets: toast,
+    translateY: "250px",
+    duration: 500,
+    easing: "easeInOutExpo",
+  });
+}
+
+copyClipBoard.addEventListener("mouseenter", HideOffIcon);
+copyClipBoard.addEventListener("mouseleave", HideOnIcon);
+copyClipBoard.addEventListener("click", Copy);
+
+function HideOffIcon() {
+  anime({
+    targets: copyIcon,
+    opacity: 1,
+    duration: 300,
+    delay: 150,
+    easing: "easeInOutExpo",
+  });
+
+  anime({
+    targets: copyClipBoard,
+    width: "225px",
+    duration: 500,
+    easing: "easeInOutExpo",
+  });
+}
+
+function HideOnIcon() {
+  anime({
+    targets: copyIcon,
+    opacity: 0,
+    duration: 300,
+    easing: "easeInOutExpo",
+  });
+
+  anime({
+    targets: copyClipBoard,
+    width: "200px",
+    duration: 500,
+    easing: "easeInOutExpo",
+  });
+}
+
+function Copy() {
+  var email = document.getElementById("mail_copy").innerHTML;
+  navigator.clipboard.writeText(email);
+
+  copyClipBoard.classList.add("w-225px");
+  copyIcon.classList.remove("icone_copy");
+  checkIcon.classList.remove("d-none");
+}
