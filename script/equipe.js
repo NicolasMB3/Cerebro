f8a10aad3d9d7b5f121608f5264a86d094f49a88
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2023-10-30 16:40:13 +0100

    Page de panier :+1:

--- script/equipe.js
+++ script/equipe.js
@@ -1,9 +1,18 @@
 const containerLogo = document.getElementById("container_logo");
+const logoImages = document.querySelectorAll(".container_logo img");
+const imageWidth = logoImages[0].clientWidth; // Largeur d'une image
+
+containerLogo.innerHTML += containerLogo.innerHTML;
 
 anime({
   targets: containerLogo,
-  translateX: "-100%",
-  duration: 35000,
+  translateX: -imageWidth * logoImages.length,
+  duration: 95000,
   loop: true,
   easing: "linear",
+  update: (anim) => {
+    if (anim.progress >= 1) {
+      containerLogo.style.transform = "translateX(0)";
+    }
+  },
 });
